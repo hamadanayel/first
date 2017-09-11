da26d7ca7fda6025f30028f80e1be4b87a17382a
hamadanayel <31835559+hamadanayel@users.noreply.github.com>
2017-09-11 12:44:21 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,116 +1,13 @@
-Jean B-PER
-Pierre I-PER
-lives O
+Number O
+of O
+glucocorticoid B-protein
+receptors I-protein
 in O
-New B-LOC
-York I-LOC
-. O
-
-The O
-European B-ORG
-Union I-ORG
-is O
-a O
-political O
+lymphocytes B-cell_type
 and O
-economic O
-union O
-
-A O
-French B-MISC
-American I-MISC
-actor O
-won O
-an O
-oscar O
-
-Jean B-PER
-Pierre I-PER
-lives O
-in O
-New B-LOC
-York I-LOC
+their O
+sensitivity O
+to O
+hormone O
+action O
 . O
-
-The O
-European B-ORG
-Union I-ORG
-is O 
-a O 
-political O
-and O
-economic O
-union O
-
-A O
-French B-MISC
-American I-MISC
-actor O
-won O
-an O
-oscar O
-
-Jean B-PER
-Pierre I-PER
-lives O
-in O
-New B-LOC
-York I-LOC
-. O
-
-The O
-European B-ORG
-Union I-ORG
-is O 
-a O 
-political O
-and O
-economic O
-union O
-
-A O
-French B-MISC
-American I-MISC
-actor O
-won O
-an O
-oscar O
-
-Jean B-PER
-Pierre I-PER
-lives O
-in O
-New B-LOC
-York I-LOC
-. O
-
-The O
-European B-ORG
-Union I-ORG
-is O 
-a O 
-political O
-and O
-economic O
-union O
-
-A O
-French B-MISC
-American I-MISC
-actor O
-won O
-an O
-oscar O
-Number	O
-of	O
-glucocorticoid	B-protein
-receptors	I-protein
-in	O
-lymphocytes	B-cell_type
-and	O
-their	O
-sensitivity	O
-to	O
-hormone	O
-action	O
-.	O
